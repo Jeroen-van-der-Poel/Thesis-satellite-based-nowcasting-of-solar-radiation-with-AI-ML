b3772b49e5094428126e5496c822de26493cb0fb
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-04-01 11:04:31 +0200

    count tf records

--- Data/data_control.py
+++ Data/data_control.py
@@ -12,13 +12,24 @@ def get_all_nc_files(path):
                 all_file_name_list.append(file)
     return all_file_name_list
 
+def count_tfrecord_files(tfrecord_dir):
+    total_samples = 0
+    tfrecord_files = [f for f in os.listdir(tfrecord_dir) if f.endswith('.tfrecord')]
+
+    for tfrecord_file in tfrecord_files:
+        path = os.path.join(tfrecord_dir, tfrecord_file)
+        try:
+            for _ in tf.data.TFRecordDataset(path):
+                total_samples += 1
+        except tf.errors.DataLossError as e:
+            print(f"Skipped corrupted file: {tfrecord_file} | Error: {e}")
+    return total_samples
 
 if __name__ == "__main__":
     raw_train_data = '/net/pc200258/nobackup_1/users/meirink/Jeroen/raw_train_data'
     raw_val_data = '/net/pc200258/nobackup_1/users/meirink/Jeroen/raw_val_data'
     raw_test_data = '/net/pc200258/nobackup_1/users/meirink/Jeroen/raw_test_data'
     
-    new_data = '/net/pc200258/nobackup_1/users/meirink/Jeroen/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data'
     train_data = '/net/pc200258/nobackup_1/users/meirink/Jeroen/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/train_data'
     val_data = '/net/pc200258/nobackup_1/users/meirink/Jeroen/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/val_data'
     test_data = '/net/pc200258/nobackup_1/users/meirink/Jeroen/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/test_data'
@@ -28,3 +39,7 @@ if __name__ == "__main__":
     print(f"Total raw train data: {len(get_all_nc_files(raw_train_data))}")
     print(f"Total raw val data: {len(get_all_nc_files(raw_val_data))}")
     print(f"Total raw test data: {len(get_all_nc_files(raw_test_data))}")
+
+    print(f"Total new train data: {count_tfrecord_files(train_data)}")
+    print(f"Total new val data: {count_tfrecord_files(val_data)}")
+    print(f"Total new test data: {count_tfrecord_files(test_data)}")
