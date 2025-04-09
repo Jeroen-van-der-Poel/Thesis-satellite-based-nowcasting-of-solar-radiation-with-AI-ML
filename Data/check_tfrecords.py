c1a09dd1381198cdd1448b7160db2ccdc37ec2aa
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-04-09 16:19:48 +0200

    reverted back to remove

--- Data/check_tfrecords.py
+++ Data/check_tfrecords.py
@@ -24,12 +24,12 @@ def check_tfrecords(directory):
     
     for cor in corrupted_files:
         print(f"Corrupted file: {cor}")
-        # try:
-        #     os.remove(corrupted_files_paths[corrupted_files.index(cor)])
-        #     print(f"Removed corrupted file: {cor}")
-        # except Exception as e:
-        #     print(f"Failed to remove {cor}: {e}")
+        try:
+            os.remove(corrupted_files_paths[corrupted_files.index(cor)])
+            print(f"Removed corrupted file: {cor}")
+        except Exception as e:
+            print(f"Failed to remove {cor}: {e}")
 
-#check_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/train_data')
-#check_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/val_data')
+check_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/train_data')
+check_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/val_data')
 check_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/test_data')
