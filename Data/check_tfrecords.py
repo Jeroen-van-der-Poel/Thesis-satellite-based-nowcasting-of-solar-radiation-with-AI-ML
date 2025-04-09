85285ef892dd5e630f021bb5da3aa71b8119e564
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-04-09 13:38:15 +0200

    check update

--- Data/check_tfrecords.py
+++ Data/check_tfrecords.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import os
 
 def check_tfrecords(directory):
+    corrupted_files = []
     for file in os.listdir(directory):
         if file.endswith('.tfrecords'):
             path = os.path.join(directory, file)
@@ -11,9 +12,14 @@ def check_tfrecords(directory):
                     pass
             except tf.errors.DataLossError as e:
                 print(f"Corrupted: {file} | {e}")
+                corrupted_files.append(file)
             except Exception as e:
                 print(f"Other issue in {file}: {e}")
+                corrupted_files.append(file)
             else:
                 print(f"OK: {file}")
+    
+    for cor in corrupted_files:
+        print(f"Corrupted file: {cor}")
 
 check_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/train_data')
