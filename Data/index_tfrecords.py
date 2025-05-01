4df497409204e32b690b22244bcb6a2290084c62
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-05-01 15:04:09 +0200

    gzip fix

--- Data/index_tfrecords.py
+++ Data/index_tfrecords.py
@@ -10,11 +10,18 @@ def index_tfrecords(tfrecord_dir):
         index_file = tf_file + '.index'
         if not os.path.exists(index_file):
             print(f"Indexing {tf_file}...")
-            tfrecord2idx.create_index(tf_file, index_file)
+            tfrecord2idx.create_index(tf_file, index_file, compression_type='GZIP')
         else:
             print(f"Index already exists for {tf_file}")
 
+def remove_index_files(tfrecord_dir):
+    index_files = glob(os.path.join(tfrecord_dir, '*.tfrecords.index'))
+    for idx_file in index_files:
+        print(f"Removing {idx_file}")
+        os.remove(idx_file)
+
 if __name__ == "__main__":
     #index_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/train_data')
-    #index_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/val_data')
-    index_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/test_data')
+    remove_index_files('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/val_data')
+    index_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/val_data')
+    #index_tfrecords('/data1/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/Data/test_data')
