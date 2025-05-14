b9f62695400e2fa6ed529b595f7a50d924718724
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-05-14 10:39:10 +0200

    fix?

--- DGMR-SO/train.py
+++ DGMR-SO/train.py
@@ -4,7 +4,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.mixed_precision import LossScaleOptimizer, set_global_policy
+from tensorflow.keras import mixed_precision
 from Data.data_pipeline import Dataset
 from model.dgmr import DGMR
 from utils.losses import Loss_hing_disc, Loss_hing_gen
@@ -12,8 +12,7 @@ import os
 from utils.utils import *
 
 # Enables mixed precision
-set_global_policy('mixed_float16') 
-
+mixed_precision.set_global_policy('mixed_float16')
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
@@ -47,12 +46,9 @@ print("Log directory:", str(ROOT / "logs" / (str(MODEL_NAME) + '_v' + str(MODEL_
 prof_dir = str(ROOT / "logs" / (str(MODEL_NAME) + '_v' + str(MODEL_VERSION)) / "profiler/")
 # profiler_writer = tf.summary.create_file_writer(prof_dir)
 
-# Mixed precision
-base_disc_opt = Adam(learning_rate=2E-4, beta_1=0.0, beta_2=0.999)
-base_gen_opt = Adam(learning_rate=1E-5, beta_1=0.0, beta_2=0.999)
-disc_optimizer = LossScaleOptimizer(base_disc_opt)
-gen_optimizer = LossScaleOptimizer(base_gen_opt)
 
+disc_optimizer = Adam(learning_rate=2E-4, beta_1=0.0, beta_2=0.999)
+gen_optimizer = Adam(learning_rate=1E-5, beta_1=0.0, beta_2=0.999)
 loss_hinge_gen = Loss_hing_gen()
 loss_hinge_disc = Loss_hing_disc()
 
