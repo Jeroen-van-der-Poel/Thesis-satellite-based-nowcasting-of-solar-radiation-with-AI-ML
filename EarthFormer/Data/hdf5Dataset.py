e72e45080826ae1bc276fdaa89ceebd8d55c4255
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-05-21 10:46:29 +0200

    try h5 on vm

---
+++ EarthFormer/Data/hdf5Dataset.py
@@ -0,0 +1,22 @@
+import h5py
+import torch
+from torch.utils.data import Dataset
+
+class HDF5NowcastingDataset(Dataset):
+    def __init__(self, h5_path):
+        super().__init__()
+        self.h5_path = h5_path
+        self.file = None  
+
+    def _init_file(self):
+        if self.file is None:
+            self.file = h5py.File(self.h5_path, 'r')
+            self.vil = self.file['vil']
+
+    def __len__(self):
+        self._init_file()
+        return self.vil.shape[0]
+
+    def __getitem__(self, idx):
+        self._init_file()
+        return torch.from_numpy(self.vil[idx])
