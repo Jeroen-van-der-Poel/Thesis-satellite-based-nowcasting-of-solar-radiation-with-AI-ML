91a810bf364840f0e43cc0f0d2a60a17d07d8ce0
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-05-22 11:01:46 +0200

    last float16

--- DGMR-SO/model/generator.py
+++ DGMR-SO/model/generator.py
@@ -162,7 +162,7 @@ class Sampler(snt.Module):
         # activation
         hs = [tf.nn.sigmoid(h) for h in hs]
 
-        return tf.cast(tf.stack(hs, axis=1), tf.float32)
+        return tf.cast(tf.stack(hs, axis=1), tf.float16)
 
 class GBlock(snt.Module):
     """Residual generator block without upsampling."""
