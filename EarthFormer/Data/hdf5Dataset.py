455004ace9de7d73307e16282de70f982c462bb1
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-05-27 14:23:37 +0200

    d

--- EarthFormer/Data/hdf5Dataset.py
+++ EarthFormer/Data/hdf5Dataset.py
@@ -5,12 +5,19 @@ import os
 
 class HDF5NowcastingDataset(Dataset):
     def __init__(self, h5_path):
-        with h5py.File(h5_path, 'r') as f:
-            self.vil = f['vil'][:]
-        print(f"[RAM DATASET] Loaded {len(self.vil)} samples into memory")
+        self.h5_path = h5_path
+        self._file = None
+        self._vil = None
 
-    def __len__(self):
-        return len(self.vil)
+    def _lazy_init(self):
+        if self._vil is None:
+            self._file = h5py.File(self.h5_path, 'r')
+            self._vil = self._file['vil']  # Access remains on disk
 
     def __getitem__(self, idx):
-        return torch.from_numpy(self.vil[idx])
+        self._lazy_init()
+        return torch.from_numpy(self._vil[idx])  # Still lazy-loaded
+
+    def __len__(self):
+        self._lazy_init()
+        return self._vil.shape[0]
