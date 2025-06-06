7d3d6eed5d369ffdaa27950b6c2dfee11cd3ca10
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-06-06 10:57:06 +0200

    memory

--- EarthFormer/run_ef.sh
+++ EarthFormer/run_ef.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=csedu
 #SBATCH --qos=csedu-large
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=83G
+#SBATCH --mem=56G
 #SBATCH --gres=gpu:5
 #SBATCH --time=48:00:00
 #SBATCH --output=myjob-%j.out
