703debece28670f7a2b4ee84856126346719db7b
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-06-06 10:48:46 +0200

    try without conda

--- EarthFormer/run_ef.sh
+++ EarthFormer/run_ef.sh
@@ -9,29 +9,29 @@
 #SBATCH --output=myjob-%j.out
 #SBATCH --error=myjob-%j.err
 
-module load Miniconda3  
-source ~/.bashrc
+# module load Miniconda3  
+# source ~/.bashrc
 
-ENV_NAME=ef_env
+# ENV_NAME=ef_env
 
-if ! conda env list | grep -q "$ENV_NAME"; then
-    echo "Creating conda environment: $ENV_NAME"
-    conda create -y -n $ENV_NAME python=3.9
-    conda activate $ENV_NAME
+# if ! conda env list | grep -q "$ENV_NAME"; then
+#     echo "Creating conda environment: $ENV_NAME"
+#     conda create -y -n $ENV_NAME python=3.9
+#     conda activate $ENV_NAME
 
-    # Install dependencies
-    pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
-    pip install pytorch_lightning==2.5.1
-    pip install torchmetrics==2.5.1
-    pip install xarray netcdf4 opencv-python earthnet==0.3.9
-    pip install -U -e . --no-build-isolation
-else
-    echo "Conda environment $ENV_NAME already exists."
-    conda activate $ENV_NAME
-fi
+#     # Install dependencies
+#     pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+#     pip install pytorch_lightning==2.5.1
+#     pip install torchmetrics==2.5.1
+#     pip install xarray netcdf4 opencv-python earthnet==0.3.9
+#     pip install -U -e . --no-build-isolation
+# else
+#     echo "Conda environment $ENV_NAME already exists."
+#     conda activate $ENV_NAME
+# fi
 
-cd $HOME/projects/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/EarthFormer
+# cd $HOME/projects/Thesis-satellite-based-nowcasting-of-solar-radiation-with-AI-ML/EarthFormer
 
 
 # Train the model
-python train.py --gpu 0 --cfg config/train.yml --ckpt_name last.ckpt --save ef_v1
+python train.py --gpu 1 --cfg config/train.yml --ckpt_name last.ckpt --save ef_v1
