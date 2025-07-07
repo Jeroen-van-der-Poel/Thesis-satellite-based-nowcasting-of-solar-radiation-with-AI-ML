c9c8ed68a7e596a3a3132be32746c7a1bf5d75fc
Jeroen van der Poel <jfa.vanderpoel@gmail.com>
2025-07-07 15:31:12 +0200

    removed white spaces

--- EarthFormer/visualization/sevir/sevir_vis_seq_denorm.py
+++ EarthFormer/visualization/sevir/sevir_vis_seq_denorm.py
@@ -204,7 +204,7 @@ def visualize_result_vertical(in_seq, target_seq, pred_seq_list: List[np.array],
             a.yaxis.set_ticks([])
 
     # Adjust layout and add horizontal colorbar
-    plt.subplots_adjust(hspace=0.1, wspace=0.05, bottom=0.20)
+    plt.subplots_adjust(hspace=0.1, wspace=0.05, top=0.05, bottom=0.05)
 
     cbar_ax = fig.add_axes([0.90, 0.15, 0.015, 0.7])
     cb = plt.colorbar(ScalarMappable(norm=Normalize(vmin=SSI_VMIN, vmax=SSI_VMAX),
